914db21905cd14473ceced4525ee62fa0bb7c21f
Adrián Domínguez <dominguez.obrero.adrian@iescamas.es>
2026-04-08 11:13:10 +0200

    Refactor Order class fields and methods

--- OnlineShopping/src/main/java/com/ejemplo/Order.java
+++ OnlineShopping/src/main/java/com/ejemplo/Order.java
@@ -8,130 +8,82 @@ import tipos.Date;
  * Class Order
  */
 public class Order {
-
-  //
-  // Fields
-  //
-
+  
   private String number;
   private Date ordered;
   private Date shipped;
-  private Address ship_to;
+  private Address shipTo;
   private OrderStatus status;
-  private Double total;
+  private double total;
+  private List<LineItem> items;
   
-  //
-  // Constructors
-  //
-  public Order () { };
+  public Order() {
+    items = new ArrayList<>();
+    
+    ordered = new Date();
+    status = OrderStatus.NEW;
+  }
   
-  //
-  // Methods
-  //
-
-
-  //
-  // Accessor methods
-  //
-
-  /**
-   * Set the value of number
-   * @param newVar the new value of number
-   */
-  public void setNumber (String newVar) {
-    number = newVar;
+  public Order(String number, Address shipTo) {
+    this();
+    this.number = number;
+    this.shipTo = shipTo;
   }
-
-  /**
-   * Get the value of number
-   * @return the value of number
-   */
-  public String getNumber () {
-    return number;
+  
+  public String getNumber() { 
+    return number; 
   }
-
-  /**
-   * Set the value of ordered
-   * @param newVar the new value of ordered
-   */
-  public void setOrdered (Date newVar) {
-    ordered = newVar;
+  
+  public void setNumber(String number) { 
+    this.number = number; 
   }
-
-  /**
-   * Get the value of ordered
-   * @return the value of ordered
-   */
-  public Date getOrdered () {
-    return ordered;
+  
+  public Date getOrdered() { 
+    return ordered; 
   }
-
-  /**
-   * Set the value of shipped
-   * @param newVar the new value of shipped
-   */
-  public void setShipped (Date newVar) {
-    shipped = newVar;
+  
+  public Date getShipped() { 
+    return shipped; 
   }
-
-  /**
-   * Get the value of shipped
-   * @return the value of shipped
-   */
-  public Date getShipped () {
-    return shipped;
+  
+  public void setShipped(Date shipped) { 
+    this.shipped = shipped; 
   }
-
-  /**
-   * Set the value of ship_to
-   * @param newVar the new value of ship_to
-   */
-  public void setShip_to (Address newVar) {
-    ship_to = newVar;
+  
+  public Address getShipTo() { 
+    return shipTo; 
   }
-
-  /**
-   * Get the value of ship_to
-   * @return the value of ship_to
-   */
-  public Address getShip_to () {
-    return ship_to;
+  
+  public void setShipTo(Address shipTo) { 
+    this.shipTo = shipTo; 
   }
-
-  /**
-   * Set the value of status
-   * @param newVar the new value of status
-   */
-  public void setStatus (OrderStatus newVar) {
-    status = newVar;
+  
+  public OrderStatus getStatus() { 
+    return status; 
   }
-
-  /**
-   * Get the value of status
-   * @return the value of status
-   */
-  public OrderStatus getStatus () {
-    return status;
+  
+  public void setStatus(OrderStatus status) { 
+    this.status = status; 
   }
-
-  /**
-   * Set the value of total
-   * @param newVar the new value of total
-   */
-  public void setTotal (Double newVar) {
-    total = newVar;
+  
+  public double getTotal() { 
+    return total; 
   }
-
-  /**
-   * Get the value of total
-   * @return the value of total
-   */
-  public Double getTotal () {
-    return total;
+  
+  public List<LineItem> getItems() { 
+    return items; 
   }
-
-  //
-  // Other methods
-  //
+  
+  public void addItem(LineItem item) {
+    items.add(item);
+    recalculateTotal();
+  }
+  
+  public void recalculateTotal() {
+    total = items.stream()
+    .mapToDouble(i -> i.getPrice() * i.getQuantity())
+    .sum();
+  
+  }
 
 }
