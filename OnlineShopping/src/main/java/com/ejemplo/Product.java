66b4c7b0fffbdc7517a1ead89adace5ce2bfd73d
Adrián Domínguez <dominguez.obrero.adrian@iescamas.es>
2026-04-08 11:57:11 +0200

    Refactor Product class to use String for supplier

--- OnlineShopping/src/main/java/com/ejemplo/Product.java
+++ OnlineShopping/src/main/java/com/ejemplo/Product.java
@@ -7,78 +7,39 @@ import tipos.Supplier;
  */
 public class Product {
 
-  //
-  // Fields
-  //
-
   private String id;
   private String name;
-  private Supplier supplier;
+  private String supplier;
   
-  //
-  // Constructors
-  //
-  public Product () { };
+  public Product() {}
   
-  //
-  // Methods
-  //
-
-
-  //
-  // Accessor methods
-  //
-
-  /**
-   * Set the value of id
-   * @param newVar the new value of id
-   */
-  public void setId (String newVar) {
-    id = newVar;
+  public Product(String id, String name, String supplier) {
+    this.id = id;
+    this.name = name;
+    this.supplier = supplier;
   }
-
-  /**
-   * Get the value of id
-   * @return the value of id
-   */
-  public String getId () {
-    return id;
+  
+  public String getId() { 
+    return id; 
   }
-
-  /**
-   * Set the value of name
-   * @param newVar the new value of name
-   */
-  public void setName (String newVar) {
-    name = newVar;
+  
+  public void setId(String id) { 
+    this.id = id; 
   }
-
-  /**
-   * Get the value of name
-   * @return the value of name
-   */
-  public String getName () {
-    return name;
+  
+  public String getName() { 
+    return name; 
   }
-
-  /**
-   * Set the value of supplier
-   * @param newVar the new value of supplier
-   */
-  public void setSupplier (Supplier newVar) {
-    supplier = newVar;
+  
+  public void setName(String name) { 
+    this.name = name; 
   }
-
-  /**
-   * Get the value of supplier
-   * @return the value of supplier
-   */
-  public Supplier getSupplier () {
-    return supplier;
+  
+  public String getSupplier() { 
+    return supplier; 
   }
-
-  //
-  // Other methods
-  //
-
+  
+  public void setSupplier(String supplier) { 
+    this.supplier = supplier; 
+  }
 }
