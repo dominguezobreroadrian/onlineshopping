3cad12b62d00aae86e692b5dba3d2573d3681861
Adrián Domínguez <dominguez.obrero.adrian@iescamas.es>
2026-04-08 12:02:26 +0200

    Update Date.java

--- OnlineShopping/src/main/java/tipos/Date.java
+++ OnlineShopping/src/main/java/tipos/Date.java
@@ -1,33 +1,59 @@
 package tipos;
 
-
-
 /**
  * Class Date
  */
 public class Date {
 
-  //
-  // Fields
-  //
+  private int day;
+  private int month;
+  private int year;
 
-  
-  //
-  // Constructors
-  //
-  public Date () { };
-  
-  //
-  // Methods
-  //
+  public Date() { }
 
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
 
-  //
-  // Accessor methods
-  //
+  public int getDay() {
+    return day;
+  }
 
-  //
-  // Other methods
-  //
+  public void setDay(int day) {
+    this.day = day;
+  }
 
+  public int getMonth() {
+    return month;
+  }
+
+  public void setMonth(int month) {
+    this.month = month;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public String format() {
+    return day + "/" + month + "/" + year;
+  }
+
+  @Override
+  public String toString() {
+    return format();
+  }
+
+  public boolean isBefore(Date other) {
+    if (this.year < other.year) return true;
+    if (this.year == other.year && this.month < other.month) return true;
+    if (this.year == other.year && this.month == other.month && this.day < other.day) return true;
+    return false;
+  }
 }
